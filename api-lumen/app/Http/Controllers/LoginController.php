bc5102c8ef394885db7870f3345cca7b8ca7b3d8
bagusjackal <76152181+bagusjackal@users.noreply.github.com>
2022-06-19 02:59:42 +0700

    api lumen login

--- api-lumen/app/Http/Controllers/LoginController.php
+++ api-lumen/app/Http/Controllers/LoginController.php
@@ -38,24 +38,33 @@ class LoginController extends Controller
 
     $user = User::where('email',$email)->first();
 
-    if ($user->password === $password) {
-        $token = Str::random(40);
-
-        $user->update([
-            'api_token' => $token
-        ]);
-
-        return response()->json([
-            'pesan' => 'login berhasil',
-            'token' => $token,
-            'data' => $user
-        ]);
-    }else {
-        return response()->json([
-            'pesan' => 'login gagal',
-            'data' => ''
-        ]);
+    if ($user->status === 1) {
+        if ($user->password === $password) {
+            $token = Str::random(40);
+    
+            $user->update([
+                'api_token' => $token
+            ]);
+    
+            return response()->json([
+                'pesan' => 'login berhasil',
+                'token' => $token,
+                'data' => $user
+            ]);
+        } else {
+            return response()->json([
+                'pesan' => 'login gagal',
+                'data' => ''
+            ]);
+        }
+    } else {
+            return response()->json([
+                'pesan' => 'login gagal',
+                'data' => ''
+            ]);
     }
+
+   
    }
 
    public function update(Request $request, $id)
